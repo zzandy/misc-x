1c5703d87ce056c1458fc8fd3c1b7022cc2b223c
zzandy <zzandy@gmail.com>
2018-07-16 16:30:33 +0200

    add non-integer random support

--- lib/rnd.ts
+++ lib/rnd.ts
@@ -1,14 +1,18 @@
 export function rnd(): number;
 export function rnd<T>(a: T[]): T;
 export function rnd(a: number): number;
-export function rnd(a: number, b: number): number;
-export function rnd<T>(a?: T[] | number, b?: number): T | number {
+export function rnd(a: number, noint: boolean): number;
+export function rnd(a: number, b: number, noint: boolean): number;
+export function rnd<T>(a?: T[] | number, b?: number | boolean, c?: boolean): T | number {
     if (a !== undefined && b !== undefined) {
-        return (b + (<number>a - b) * Math.random()) | 0;
-    }
-    else if (a !== undefined && b === undefined) {
+        if (c !== undefined)
+            return <number>b + (<number>a - <number>b) * Math.random();
+
+        return typeof b === "boolean"
+            ? <number>a * Math.random()
+            : (b + (<number>a - b) * Math.random()) | 0;
+    } else if (a !== undefined && b === undefined) {
         if (a instanceof Array) return a[(Math.random() * a.length) | 0];
         else return (a * Math.random()) | 0;
-    }
-    else return Math.random();
+    } else return Math.random();
 }
