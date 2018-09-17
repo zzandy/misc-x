46d1f2549dc15f7cdc8fc64c8ba7f13cb6258e2d
zzandy <zzandy@gmail.com>
2018-09-17 16:58:46 +0200

    [nrg] a bit of interactivity

--- nrg/dist/bundle.js
+++ nrg/dist/bundle.js
@@ -1,18 +1,38 @@
 System.register("main", ["vue"], function (exports_1, context_1) {
     "use strict";
-    var vue_1, start;
+    var vue_1, max, start;
     var __moduleName = context_1 && context_1.id;
     function gen(isOn, name, yld, maxFuel, topUp) {
         if (topUp === void 0) { topUp = maxFuel / 20; }
         return {
             isOn: isOn == 1,
-            name: name, yield: yld, fuel: maxFuel, maxFuel: maxFuel, fuelTopUp: topUp
+            name: name,
+            yield: yld,
+            fuel: maxFuel,
+            maxFuel: maxFuel,
+            fuelTopUp: topUp
         };
     }
     function wait(n) {
         return new Promise(function (resolve, reject) {
             setTimeout(resolve, n);
         });
+    }
+    function loop(ups, update) {
+        var fixedDelta = 1000 / ups;
+        var acc = fixedDelta;
+        var then = Date.now();
+        var tick = function () {
+            var now = Date.now();
+            acc += now - then;
+            then = now;
+            while (acc >= fixedDelta) {
+                acc -= fixedDelta;
+                update(fixedDelta);
+            }
+            window.requestAnimationFrame(tick);
+        };
+        tick();
     }
     return {
         setters: [
@@ -21,36 +41,47 @@ System.register("main", ["vue"], function (exports_1, context_1) {
             }
         ],
         execute: function () {
+            max = Math.max;
             exports_1("start", start = function () {
-                var uranium = [1000, { name: 'Uranium' }];
+                var uranium = [1000, { name: "Uranium" }];
                 var data = {
-                    store: [uranium, [10000, { name: 'Iron Ore' }], [3000, { name: 'Coal' }]],
+                    store: [
+                        uranium,
+                        [10000, { name: "Iron Ore" }],
+                        [3000, { name: "Coal" }]
+                    ],
                     generators: [
-                        gen(1, 'main', 1000, 1000),
-                        gen(0, 'aux #1', 200, 1000),
-                        gen(0, 'aux #2', 200, 1000),
-                        gen(0, 'aux #3', 200, 1000),
-                        gen(0, 'aux #4', 200, 1000),
-                        gen(0, 'aux #5', 200, 1000)
+                        gen(1, "main", 1000, 1000),
+                        gen(0, "aux #1", 200, 1000),
+                        gen(0, "aux #2", 200, 1000),
+                        gen(0, "aux #3", 200, 1000),
+                        gen(0, "aux #4", 200, 1000),
+                        gen(0, "aux #5", 200, 1000)
                     ]
                 };
                 var app = new vue_1.default({
-                    el: '#doc',
+                    el: "#doc",
                     data: data,
                     methods: {
                         boost: function () {
                             data.generators[0].yield *= 1.2;
-                            wait(2000).then(function () { return data.generators[0].yield /= 1.2; });
+                            wait(2000).then(function () { return (data.generators[0].yield /= 1.2); });
                         },
                         fill: function (g) {
                             g.fuel += g.fuelTopUp;
                             uranium[0] -= g.fuelTopUp;
                         },
                         canFill: function (g) {
-                            return g.maxFuel - g.fuel > g.fuelTopUp && uranium[0] > g.fuelTopUp;
+                            return (g.maxFuel - g.fuel > g.fuelTopUp && uranium[0] > g.fuelTopUp);
                         }
                     }
                 });
+                var update = function (delta) {
+                    data.generators
+                        .filter(function (gen) { return gen.fuel > 0; })
+                        .forEach(function (gen) { return (gen.fuel = max(0, gen.fuel - 0.1)); });
+                };
+                loop(60, update);
             });
         }
     };
--- nrg/src/main.ts
+++ nrg/src/main.ts
@@ -1,52 +1,113 @@
-import Vue from 'vue';
+import Vue from "vue";
+
+const max = Math.max;
 
 type Quantity = [number, Asset];
 type Asset = { name: string };
-type Generator = { name: string, isOn: boolean, yield: number, fuel: number, maxFuel: number, fuelTopUp: number };
-type Machine = { name: string, isOn: boolean, draw: number, inputs: Quantity[], products: Quantity[] };
+type Generator = {
+    name: string;
+    isOn: boolean;
+    yield: number;
+    fuel: number;
+    maxFuel: number;
+    fuelTopUp: number;
+};
+type Machine = {
+    name: string;
+    isOn: boolean;
+    draw: number;
+    inputs: Quantity[];
+    products: Quantity[];
+};
 
 export const start = () => {
-    const uranium: Quantity = [1000, { name: 'Uranium' }];
+    const uranium: Quantity = [1000, { name: "Uranium" }];
     const data = {
-        store: [uranium, [10000, { name: 'Iron Ore' }], [3000, { name: 'Coal' }]],
+        store: [
+            uranium,
+            [10000, { name: "Iron Ore" }],
+            [3000, { name: "Coal" }]
+        ],
         generators: [
-            gen(1, 'main', 1000, 1000, ),
-            gen(0, 'aux #1', 200, 1000),
-            gen(0, 'aux #2', 200, 1000),
-            gen(0, 'aux #3', 200, 1000),
-            gen(0, 'aux #4', 200, 1000),
-            gen(0, 'aux #5', 200, 1000)
+            gen(1, "main", 1000, 1000),
+            gen(0, "aux #1", 200, 1000),
+            gen(0, "aux #2", 200, 1000),
+            gen(0, "aux #3", 200, 1000),
+            gen(0, "aux #4", 200, 1000),
+            gen(0, "aux #5", 200, 1000)
         ]
     };
 
     const app = new Vue({
-        el: '#doc',
+        el: "#doc",
         data,
         methods: {
             boost() {
                 data.generators[0].yield *= 1.2;
-                wait(2000).then(() => data.generators[0].yield /= 1.2);
+                wait(2000).then(() => (data.generators[0].yield /= 1.2));
             },
             fill(g: Generator) {
                 g.fuel += g.fuelTopUp;
                 uranium[0] -= g.fuelTopUp;
             },
             canFill(g: Generator) {
-                return g.maxFuel - g.fuel > g.fuelTopUp && uranium[0] > g.fuelTopUp;
+                return (
+                    g.maxFuel - g.fuel > g.fuelTopUp && uranium[0] > g.fuelTopUp
+                );
             }
         }
     });
+
+    const update = (delta: number) => {
+        data.generators
+            .filter(gen => gen.fuel > 0)
+            .forEach(gen => (gen.fuel = max(0, gen.fuel - 0.1)));
+    };
+
+    loop(60, update);
 };
 
-function gen(isOn: 1 | 0, name: string, yld: number, maxFuel: number, topUp: number = maxFuel / 20): Generator {
+function gen(
+    isOn: 1 | 0,
+    name: string,
+    yld: number,
+    maxFuel: number,
+    topUp: number = maxFuel / 20
+): Generator {
     return {
         isOn: isOn == 1,
-        name, yield: yld, fuel: maxFuel, maxFuel, fuelTopUp: topUp
-    }
+        name,
+        yield: yld,
+        fuel: maxFuel,
+        maxFuel,
+        fuelTopUp: topUp
+    };
 }
 
 function wait(n: number): Promise<void> {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, n);
     });
+}
+
+function loop(ups: number, update: (delta: number) => void): void {
+    const fixedDelta = 1000 / ups;
+    let acc = fixedDelta;
+
+    let then = Date.now();
+
+    const tick = () => {
+        const now = Date.now();
+        acc += now - then;
+        then = now;
+
+        while (acc >= fixedDelta) {
+            acc -= fixedDelta;
+            update(fixedDelta);
+        }
+
+        window.requestAnimationFrame(tick);
+    };
+
+    tick();
 }
